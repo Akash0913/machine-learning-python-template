95db619dd7c61abc2d725ce221735d05001e9aba
Akash0913 <44508860+Akash0913@users.noreply.github.com>
2024-12-05 03:01:47 +0000

    First commit

--- src/app.py
+++ src/app.py
@@ -1,4 +1,95 @@
 from utils import db_connect
 engine = db_connect()
+import pandas as pd
+from dotenv import load_dotenv
+from sklearn.preprocessing import MinMaxScaler
+from sklearn.feature_selection import chi2, SelectKBest
+from sklearn.model_selection import train_test_split
+from sklearn.linear_model import LogisticRegression
+from sklearn.metrics import accuracy_score
+from sklearn.model_selection import GridSearchCV
+import warnings
 
-# your code here
+total_data = pd.read_csv("https://raw.githubusercontent.com/4GeeksAcademy/logistic-regression-project-tutorial/main/bank-marketing-campaign-data.csv", sep = ";")
+print(total_data.head())
+total_data = total_data.drop_duplicates().reset_index(drop = True)
+print(total_data.head())
+print(total_data.isnull().sum())
+total_data["job_n"] = pd.factorize(total_data["job"])[0]
+total_data["marital_n"] = pd.factorize(total_data["marital"])[0]
+total_data["education_n"] = pd.factorize(total_data["education"])[0]
+total_data["default_n"] = pd.factorize(total_data["default"])[0]
+total_data["housing_n"] = pd.factorize(total_data["housing"])[0]
+total_data["loan_n"] = pd.factorize(total_data["loan"])[0]
+total_data["contact_n"] = pd.factorize(total_data["contact"])[0]
+total_data["month_n"] = pd.factorize(total_data["month"])[0]
+total_data["day_of_week_n"] = pd.factorize(total_data["day_of_week"])[0]
+total_data["poutcome_n"] = pd.factorize(total_data["poutcome"])[0]
+total_data["y_n"] = pd.factorize(total_data["y"])[0]
+num_variables = ["job_n", "marital_n", "education_n", "default_n", "housing_n", "loan_n", "contact_n", "month_n", "day_of_week_n", "poutcome_n",
+                 "age", "duration", "campaign", "pdays", "previous", "emp.var.rate", "cons.price.idx", "cons.conf.idx", "euribor3m", "nr.employed", "y_n"]
+
+scaler = MinMaxScaler()
+scal_features = scaler.fit_transform(total_data[num_variables])
+total_data_scal = pd.DataFrame(scal_features, index = total_data.index, columns = num_variables)
+print(total_data_scal.head())
+X = total_data_scal.drop("y_n", axis = 1)
+y = total_data_scal["y_n"]
+
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = 0.1, random_state = 42)
+
+selection_model = SelectKBest(chi2, k = 5)
+selection_model.fit(X_train, y_train)
+ix = selection_model.get_support()
+X_train_sel = pd.DataFrame(selection_model.transform(X_train), columns = X_train.columns.values[ix])
+X_test_sel = pd.DataFrame(selection_model.transform(X_test), columns = X_test.columns.values[ix])
+
+print(X_train_sel.head())
+print(X_test_sel.head())
+
+X_train_sel["y_n"] = list(y_train)
+X_test_sel["y_n"] = list(y_test)
+X_train_sel.to_csv("clean_train.csv", index=False)
+X_test_sel.to_csv("clean_test.csv", index=False)
+train_data = pd.read_csv("clean_train.csv")
+test_data = pd.read_csv("clean_test.csv")
+
+print(train_data.head())
+
+X_train = train_data.drop(["y_n"], axis = 1)
+y_train = train_data["y_n"]
+X_test = test_data.drop(["y_n"], axis = 1)
+y_test = test_data["y_n"]
+
+model = LogisticRegression()
+print(model.fit(X_train, y_train))
+
+y_pred = model.predict(X_test)
+print(y_pred)
+
+print(accuracy_score(y_test, y_pred))
+
+hyperparams = {
+    "C": [0.001, 0.01, 0.1, 1, 10, 100, 1000],
+    "penalty": ["l1", "l2", "elasticnet", None],
+    "solver": ["newton-cg", "lbfgs", "liblinear", "sag", "saga"]
+}
+
+grid = GridSearchCV(model, hyperparams, scoring = "accuracy", cv = 10)
+print(grid)
+
+def warn(*args, **kwargs):
+    pass
+warnings.warn = warn
+
+grid.fit(X_train, y_train)
+
+print("Best hyperparameters: {grid.best_params_}")
+
+model = LogisticRegression(C = 0.1, penalty = "l2", solver = "liblinear")
+model.fit(X_train, y_train)
+
+y_pred = model.predict(X_test)
+print(y_pred)
+
+print(accuracy_score(y_test, y_pred))
